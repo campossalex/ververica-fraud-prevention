d77c442733179717a0f6ee37c7461d26cc46374a
campossalex <32500181+campossalex@users.noreply.github.com>
2026-03-14 15:12:46 -0300

    Update manual_setup.sh

--- manual_setup.sh
+++ manual_setup.sh
@@ -1,14 +1,6 @@
     #!/bin/bash
     set -euxo pipefail
 
-    # Install git
-    yum update -y
-    yum install -y git
-
-    # Clone the repo
-    cd /root
-    git clone https://github.com/campossalex/ververica-fraud-prevention ververica-platform-playground
-
     # Run pre-install script
     sudo ./ververica-platform-playground/pre-install.sh
 
